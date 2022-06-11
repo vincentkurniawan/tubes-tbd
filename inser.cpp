c10b8a8e77f9cff9420f9c2dc9d0a91f9640da14
vincentkurniawan <6181901024@student.unpar.ac.id>
2022-06-11 23:35:55 +0700

    generate random register sp exec

---
+++ inser.cpp
@@ -0,0 +1,94 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string randUser (int len) {
+    char consonents[] = {'b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z'};
+    char vowels[] = {'a','e','i','o','u'};
+    string res = "";
+
+    for (int i=0; i<len; i++){
+        if (i%2 == 0){
+            res += consonents[rand()%21];
+        }
+        else {
+            res += vowels[rand()%5];
+        }
+    }
+    return res;
+}
+
+string randNama (int len) {
+    char consonents[] = {'b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z'};
+    char vowels[] = {'a','e','i','o','u'};
+    string res = "";
+
+    for (int i=0; i<len; i++){
+        if (i%2 == 0){
+            res += consonents[rand()%21];
+        }
+        else {
+            res += vowels[rand()%5];
+        }
+    }
+    res += ' ';
+    for (int i=0; i<len; i++){
+        if (i%2 == 0){
+            res += consonents[rand()%21];
+        }
+        else {
+            res += vowels[rand()%5];
+        }
+    }
+    return res;
+}
+
+string randKota () {
+    string kota[] = 
+    {
+        "Kabul", "Kandahar", "Pretoria", "Cape Town", "Bangui",
+        "Libreville", "Tirana", "Gramsh", "Washington D.C", "New York",
+        "Canberra","Sydney","Riyadh","Mekkah","Buenos Aires",
+        "Rosario","Wina","Baden","Amsterdam","Rotterdam",
+        "Brussel","Eeklo","Brasilia","Sao Paolo","Seria",
+        "Bandar Seri Begawan","Beijing","Shanghai","Kopenhagen","Aarhus",
+        "Santo Domingo","Azua","San Salvador","Santa Ana","Alaminos",
+        "Angeles","Helsinki","Espoo","Conakry","Kissidougou",
+        "Budapest","Esztergom","Mumbai","New Delhi","jakarta",
+        "Bandung","Bagdad","Basra","Isfahan","Ray",
+        "Yerusalem","Tel Aviv","Roma","Venesia","Hverageroi",
+        "Vik i Myrdal","Kingston","Port Royal","Tokyo","Osaka",
+        "Berlin","Hamburg","Phnom Penh","Sihanoukville","Toronto",
+        "Kota Quebec","Bogota","Medellin","Seoul","Busan",
+        "Pyongyang","Nampo","San Jose","Alajuela","Vohemar",
+        "Ambalavao","Kuala Lumpur","Kuching","Guadalajara","Zacatecas",
+        "yangoon","Bago","Kathmandu","Patan","Benin City",
+        "Lagos","Oslo","Bergen","Paris","Marseille",
+        "Lisboa","Porto","Doha","Dukhan","Moskow",
+        "Kazan","Rochor","Seletar","Bangkok","Pattaya"
+    };
+
+    return kota[rand() % 100];
+}
+
+string randTelp (int len){
+    string res = "";
+    char telp [] = {'0','1','2','3','4','5','6','7','8','9'};
+    for (int i=0; i<len; i++){
+        res += telp[rand()%10];
+    }
+    return res;
+}
+
+int main () {
+    ofstream myfile;
+    myfile.open ("example.txt");
+    for (int i=0; i<50; i++){
+        string user = randUser(4);
+        string pass = randUser(8);
+        string nama = user + " " + randUser(6);
+        string email = user + "@gmail.com";
+        myfile << "EXEC register '" << user << "', '" << pass << "', '" << nama << "', '" << email << "', '" << randTelp(12) << "', 'Jl. Juanda No. " << i << "', '" << randKota() << "'\n";
+    }
+    myfile.close();
+}
--- procedures/member_penulis_lihat_artikel_dan_status.sql
+++ procedures/member_penulis_lihat_artikel_dan_status.sql
@@ -2,25 +2,10 @@ GO
 USE Perpustakaan
 
 GO
-CREATE FUNCTION member_get_artikel (@id_member INT)
-RETURNS @res TABLE 
-(
-    id_artikel INT,
-    is_premium INT,
-    nama_artikel VARCHAR(500),
-    path_artikel VARCHAR(500),
-    status_validasi INT,
-    tanggal_tulis DATE,
-    id_penulis INT,
-    id_admin INT
-)
-BEGIN
-    INSERT INTO @res
+CREATE PROCEDURE penulis_lihat_artikelnya (@id_member INT)
+AS
     SELECT id_artikel, is_premium, nama_artikel, path_artikel, status_validasi, tanggal_tulis, id_penulis, id_admin
     FROM dbo.Artikel WHERE Artikel.id_penulis = @id_member
-    RETURN
-END
 
 GO
-SELECT *
-FROM dbo.member_get_artikel (1)
+EXEC penulis_lihat_artikelnya 2
