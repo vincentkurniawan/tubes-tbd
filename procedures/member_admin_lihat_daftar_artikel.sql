91774010c82d5827fd6c1c5fe653e85c30ecd963
Nadia Clarissa Hermawan <6181901013@student.unpar.ac.id>
2022-06-11 12:28:57 +0700

    +beberapa yg kurang

---
+++ procedures/member_admin_lihat_daftar_artikel.sql
@@ -0,0 +1,18 @@
+/**
+lihat seluruh daftar artikel yang tersedia
+**/
+
+GO
+USE Perpustakaan
+
+GO
+CREATE PROCEDURE member_lihat_daftar_artikel
+AS
+	SELECT 
+		id_artikel, nama_artikel, is_premium, path_artikel, tanggal_tulis
+	FROM 
+		Artikel
+GO
+
+GO
+EXEC member_lihat_daftar_artikel 
